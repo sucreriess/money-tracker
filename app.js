c0626db70589b4eab8ccc39074b13cd87ea972d2
sucreriess <shatakshibhwardwaj15@gmail.com>
2024-01-15 01:51:53 +0530

    completed

--- app.js
+++ app.js
@@ -1,8 +1,67 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT =  3000;
+// MongoDB Connection
+mongoose.connect('mongodb://localhost:27017/money-tracker');
 
-mongoose.connect('mongodb://localhost/money-tracker');
+// MongoDB Schema
+const expenseSchema = new mongoose.Schema({
+  transactionName: String,
+  amount: Number,
+});
 
+const Expense = mongoose.model('Expense', expenseSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+// Routes
+app.get('/', async (req, res) => {
+    try {
+      const expenses = await Expense.find({});
+      res.sendFile(__dirname + '/index.html');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+  
+
+  app.post('/addExpense', async (req, res) => {
+    try {
+      const newExpense = new Expense({
+        transactionName: req.body.transactionName,
+        amount: req.body.amount,
+      });
+  
+      await newExpense.save();
+      const expenses = await Expense.find({});
+    
+      // Send the updated view with the latest expenses data
+      res.redirect('/');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
+  
+app.get('/getExpenses', async (req, res) => {
+    try {
+      const expenses = await Expense.find({});
+      res.json(expenses);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+  
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
